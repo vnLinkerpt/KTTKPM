1b8dc8f86e56997b9ce16cad570b49c47170e21e
Linkerpt <38261279+vnLinkerpt@users.noreply.github.com>
2019-12-16 13:36:30 +0700

    Add files via upload

---
+++ kttkpm.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th12 16, 2019 lúc 07:35 AM
+-- Phiên bản máy phục vụ: 10.4.6-MariaDB
+-- Phiên bản PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `kttkpm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_commentmeta`
+--
+-- Error reading structure for table kttkpm.wp_commentmeta: #1932 - Table 'kttkpm.wp_commentmeta' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_commentmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_commentmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_comments`
+--
+-- Error reading structure for table kttkpm.wp_comments: #1932 - Table 'kttkpm.wp_comments' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_comments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_comments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_links`
+--
+-- Error reading structure for table kttkpm.wp_links: #1932 - Table 'kttkpm.wp_links' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_links: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_links`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_options`
+--
+-- Error reading structure for table kttkpm.wp_options: #1932 - Table 'kttkpm.wp_options' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_options: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_options`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_postmeta`
+--
+-- Error reading structure for table kttkpm.wp_postmeta: #1932 - Table 'kttkpm.wp_postmeta' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_postmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_postmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_posts`
+--
+-- Error reading structure for table kttkpm.wp_posts: #1932 - Table 'kttkpm.wp_posts' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_posts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_posts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_termmeta`
+--
+-- Error reading structure for table kttkpm.wp_termmeta: #1932 - Table 'kttkpm.wp_termmeta' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_termmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_termmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_terms`
+--
+-- Error reading structure for table kttkpm.wp_terms: #1932 - Table 'kttkpm.wp_terms' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_terms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_terms`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_term_relationships`
+--
+-- Error reading structure for table kttkpm.wp_term_relationships: #1932 - Table 'kttkpm.wp_term_relationships' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_term_relationships: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_term_relationships`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_term_taxonomy`
+--
+-- Error reading structure for table kttkpm.wp_term_taxonomy: #1932 - Table 'kttkpm.wp_term_taxonomy' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_term_taxonomy: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_term_taxonomy`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_usermeta`
+--
+-- Error reading structure for table kttkpm.wp_usermeta: #1932 - Table 'kttkpm.wp_usermeta' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_usermeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_usermeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `wp_users`
+--
+-- Error reading structure for table kttkpm.wp_users: #1932 - Table 'kttkpm.wp_users' doesn't exist in engine
+-- Error reading data for table kttkpm.wp_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `kttkpm`.`wp_users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
